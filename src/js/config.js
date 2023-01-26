ca3cd7e9699afdc02f53b8b098e94bd710365b91
Kitupala <kimmo.tuupanen@gmail.com>
2023-01-26 21:02:09 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9444f3c6-6f11-4e6c-8eb5-9ccf32cab252';
+export const MODAL_CLOSE_SEC = 2.5;
